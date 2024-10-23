7c11bb4cba78fac90588f8c00eaecf959bb4d818
Vikash pal <151984787+vikashpal28@users.noreply.github.com>
2024-10-23 21:57:30 +0530

    what is insertion and extraction in c++?

---
+++ exp2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int num1,num2;
+    cout<<"enter the number1:\n";//insertion operator
+    cin>>num1;//Extraction operator
+    cout<<"enter the number1:\n";//insertion operator
+    cin>>num2;//Extraction operator
+    cout<<"sum : "<<num1+num2;
+    return 0;
+}
+/*
+output
+enter the number1:
+1
+enter the number1:
+2
+sum : 3
+*/
