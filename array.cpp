3f219d55db4a02443dc641279063d53111bc1ee4
Vikash pal <151984787+vikashpal28@users.noreply.github.com>
2024-11-04 11:00:40 +0530

    array in c++

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[4] = {1,2,3,4};
+    int i=0;
+    // while (i<4)
+    // {
+    //     cout<<arr[i]<<endl;
+    //     i++;
+    // }
+    do
+    {
+        cout<<arr[i]<<endl;
+        i++;
+    } while (i<4);
+    
+    
+return 0;
+}
+
