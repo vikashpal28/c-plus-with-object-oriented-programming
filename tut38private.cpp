1483eb93fc92f7989e34101e8311a2b75948da1b
Vikash pal <151984787+vikashpal28@users.noreply.github.com>
2024-11-28 15:23:06 +0530

    Single Inheritance Deep Dive

---
+++ tut38private.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class base{
+    int data1;
+    public:
+    int data2;
+    void setdata();
+    int getdata1();
+    int getdata2();
+};
+void base ::setdata(){
+    data1 = 10;
+    data2 = 20;
+}
+
+int base :: getdata1(){
+    return data1;
+}
+
+int base ::getdata2(){
+    return data2;
+}
+
+class derived : private base{
+    int data3;
+    public:
+    void process();
+    void display();
+};
+
+void derived ::process(){
+    setdata();
+    data3 = data2*getdata1();
+}
+
+void derived :: display(){
+    cout<<"the data1 = "<<getdata1()<<endl;
+    cout<<"the data2 = "<<data2<<endl;
+    cout<<"the data3 = "<<data3<<endl;
+}
+
+int main(){
+    derived d1;
+    d1.process();
+    d1.display();
+return 0;
+}
